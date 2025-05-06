dbf37eea7c284586cedcf64fa30448b3d697e28d
NikaDyn <mariiaveronika.i.dyndyn@ukd.edu.ua>
2025-05-06 21:21:34 +0300

    Fourth version

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('book_list', permanent=False)),
     path('books/', include('exchange.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- exchange/admin.py
+++ exchange/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import ExchangeRequest
+from .models import Book, ExchangeOffer, ExchangeRequest
 
+admin.site.register(Book)
+admin.site.register(ExchangeOffer)
 admin.site.register(ExchangeRequest)
